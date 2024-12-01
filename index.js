81f1d5c543068ff539bcc3271d207ebb5a486c77
veer2238 <veer2238rajput@gmail.com>
2024-12-02 05:14:26 +0530

    inde

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const app = express()
+
+
+app.get('/', (req, res) => {
+res.send('Hello World!')
+})
+
+app.listen(3005, () => {
+console.log("server connected")
+})
+
